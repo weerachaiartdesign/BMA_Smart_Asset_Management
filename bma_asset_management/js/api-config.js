f2519352665c50230497cd85af21cd3de4dca6d4
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-04-27 16:55:54 +0700

    000563-Update api-config.js

--- bma_asset_management/js/api-config.js
+++ bma_asset_management/js/api-config.js
@@ -1,6 +1,6 @@
 /**
  * version 00056
  */
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwe4tDifqnXB3D9PrEXEkhjaCPbaqOTmzFJD_Rh1sGvsyP4l90jrGUz065KyJDbCrsziQ/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwVU-QjJpAIdGSi3oKK3h1kfFw3XGrkGCirgscDm-wNzM3q8AzNp_nR5vL922Yza93BxA/exec";
 
 const GAS_API_URL = WEB_APP_URL;
